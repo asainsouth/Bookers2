65b0bde6b68f6e97336ebc722aefbdaba8502b53
EC2 Default User <ec2-user@ip-172-31-94-182.ec2.internal>
2022-06-07 05:17:50 +0000

    Initial Commit

--- app/models/book.rb
+++ app/models/book.rb
@@ -6,6 +6,10 @@ class Book < ApplicationRecord
   validates :title, presence: true
   validates :body, presence: true, length: { maximum: 200 }
 
+  scope :latest, -> {order(created_at: :desc)}
+  scope :old, -> {order(created_at: :asc)}
+  scope :star_count, -> {order(star: :desc)}
+
   def get_profile_image
     unless profile_image.attached?
       file_path = Rails.root.join('app/assets/images/no_image.jpg')
